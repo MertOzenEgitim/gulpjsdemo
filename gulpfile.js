c4c195ec879798991bba15c5ac635e8ebbdfc830
Mert Özen <Mert zen>
2025-06-03 21:05:59 +0300

    final config completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,74 @@
-const gulp=require("gulp");
-const sass=require("gulp-sass")(require("sass"));
-const concat=require("gulp-concat");
-const uglify=require("gulp-uglify");
-const imagemin=require("gulp-imagemin");
-const webpack=require("webpack-stream");
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const webpack = require("webpack-stream");
+const htmlmin = require("gulp-htmlmin");
+const browserSync = require("browser-sync").create();
 
-// gulp.task("hello",function(done){
-//     console.log("Gulp çalıştı.");
-//     done();
-// });
-
-// gulp.task("default",gulp.series("hello"));
-
-// gulp.task("clear",function(done){
-//     console.log("dist clear");
-//     done();
-// });
-
-// gulp.task("scss",function(done){
-//     console.log("scss converting");
-//     done();
-// });
-
-// gulp.task("js",function(done){
-//     console.log("js converting");
-//     done();
-// });
-
-// gulp.task("build",gulp.parallel('scss','js'));
-// gulp.task("default",gulp.series('clear','build'))
-
-
-gulp.task("sass",function(){
-    return gulp.src("./src/sass/**/*.scss")
-    .pipe(sass().on('error',sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+// HTML
+gulp.task("html", function () {
+  return gulp.src("src/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("js",function(){
-    return gulp.src("./src/js/**/*.js")
+// Sass
+gulp.task("sass", function () {
+  return gulp.src("./src/sass/**/*.scss")
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// JS
+gulp.task("js", function () {
+  return gulp.src("./src/js/**/*.js")
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("images",function(){
-    return gulp.src('src/images/*',{encoding:false})
+// Bundle
+gulp.task("bundle", function () {
+  return gulp.src('./src/js/index.js')
+    .pipe(webpack({
+      mode: "production",
+      output: { filename: 'bundle.js' },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: { loader: 'babel-loader' }
+          }
+        ]
+      }
+    }))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+});
+
+// Images
+gulp.task("images", function () {
+  return gulp.src('src/images/*', { encoding: false })
     .pipe(imagemin())
     .pipe(gulp.dest('dist/images'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task("watch",function(){
-    gulp.watch('./src/sass/**/*.scss',gulp.series("sass"));
-    gulp.watch('./src/js/**/*.js',gulp.series("js"));
-    gulp.watch('./src/images/**/*',gulp.series("images"));
+// Server + Watch
+gulp.task("serve", function () {
+  browserSync.init({ server: { baseDir: "./dist" } });
+  gulp.watch('./src/sass/**/*.scss', gulp.series("sass"));
+  gulp.watch('./src/js/**/*.js', gulp.series("js", "bundle"));
+  gulp.watch('./src/images/**/*', gulp.series("images"));
+  gulp.watch('./src/index.html', gulp.series("html"));
 });
 
-gulp.task("bundle",function(){
-    return gulp.src('./src/js/index.js')
-    .pipe(webpack({
-        mode:"production",
-        output:{filename:'bundle.js'},
-        module:{
-            rules:[
-                {
-                    test:/\.js$/,
-                    exclude:/node_modules/,
-                    use:{loader:'babel-loader'}
-                }
-            ]
-        }
-    }))
-    .pipe(gulp.dest('./dist/js'));
-})
-
-gulp.task("build",gulp.parallel('sass','js','images','bundle'))
-gulp.task("default",gulp.series("build","watch"));
+// Build + Default
+gulp.task("build", gulp.parallel('html', 'sass', 'js', 'images', 'bundle'));
+gulp.task("default", gulp.series("build", "serve"));
